4f889c7191eedc3acb3396cca2d9d097c8b365ef
jodies383 <jodies383@gmail.com>
2021-04-21 12:46:03 +0200

    File adjustments

--- radioBill.js
+++ radioBill.js
@@ -1,3 +1,44 @@
-function radioBill(){
+function radioBill() {
+
+
+    var cTotal = 0;
+    var sTotal = 0;
+    var radioTotal = 0;
+
+    function calcRadio(checkedRadioBtn) {
+        
+        if (checkedRadioBtn === "call") {
+            
+            cTotal += 2.75,
+            radioTotal += 2.75;
+        }
+        else if (checkedRadioBtn === "sms") {
+            sTotal += 0.75,
+            radioTotal += 0.75;
+        }
+    }
+        function radioCall(){
+            return cTotal;
+        }
+        function radioSms(){
+            return sTotal;
+        }
+        function radioT(){
+            return radioTotal;
+        }
+        function radioBillLevels(){
+            if ((radioTotal) >= 50){
+                return "critical"
+            } else if ((radioTotal) >= 30){
+                return "warning"
+            }
+        }
     
+    return {
+        calcRadio,
+        radioCall,
+        radioSms,
+        radioT,
+        radioBillLevels
+    }
 }
--- radioBill.tests.js
+++ radioBill.tests.js
@@ -1,5 +1,72 @@
 describe('The Radio Bill Function', function () {
-    it('should add based on the call and sms entered', function () {
-        
+    it('should add R2.75 based on the call entered', function () {
+        let radioTest = radioBill();
+        radioTest.calcRadio('call');
+        radioTest.calcRadio('call');
+        radioTest.calcRadio('call');
+        assert.equal(radioTest.radioCall(), 8.25);
+    
+    });
+    it('should add R0.75 based on the sms entered', function () {
+        let radioTest = radioBill();
+        radioTest.calcRadio('sms');
+        radioTest.calcRadio('sms');
+        radioTest.calcRadio('sms');
+        assert.equal(radioTest.radioSms(), 2.25);
+    });
+    it('should equal R10.5 based on the calls and sms entered', function () {
+        let radioTest = radioBill();
+        radioTest.calcRadio('sms');
+        radioTest.calcRadio('call');
+        radioTest.calcRadio('call');
+        radioTest.calcRadio('sms');
+        radioTest.calcRadio('call');
+        radioTest.calcRadio('sms');
+        assert.equal(radioTest.radioT(), 10.5);
+    });
+    describe('Warning & Critical Level', function () {
+        it('should return a class name of "warning" since total is over R30', function () {
+            let radioTest = radioBill();
+            radioTest.calcRadio('sms');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('sms');
+            assert.equal("warning", radioTest.radioBillLevels());
+        });
+        it('should return a class name of "critical" since total is over R50', function () {
+            let radioTest = radioBill();
+            radioTest.calcRadio('sms');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('call');
+            radioTest.calcRadio('sms');
+            assert.equal("critical", radioTest.radioBillLevels());
+        });
     });
 });
